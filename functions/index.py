a5802b19d4e20175cad7faec1e67172ce09196b3
saboorkhan6 <saoorkhan6@gmail.com>
2026-08-26 11:33:04 +0530

    commit

--- functions/index.py
+++ functions/index.py
@@ -1,3 +1,8 @@
 from new import check
 a=int(input("enter a number="))
 check(a)   
+
+#return
+def add(s,t):
+    return s+t
+print(add(1,2))    
---
+++ lambda-mapping/index.py
@@ -0,0 +1,19 @@
+#lambda
+#add using lambda :
+add=lambda x,y: x+y
+print(add(1,2))
+
+#even-odd using lambda
+even_odd=lambda x:"even" if x%2==0 else "odd"
+print(even_odd(4))
+
+#map:
+#to find square of all elements in the list
+lt=[1,2,3,4,5]
+sqr=list(map(lambda i: i*i,lt))
+print(sqr)
+
+#type-changing using map
+new=['1','2','3','4']
+a=list(map(int,new))
+print(a)
