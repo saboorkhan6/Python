ba08c5e87c1814afe48280398d3f159258db7259
saboorkhan6 <saoorkhan6@gmail.com>
2026-08-26 12:35:24 +0530

    another

--- lambda-mapping/index.py
+++ lambda-mapping/index.py
@@ -1,4 +1,4 @@
-#lambda
+#lambda : for smaller tasks using keyword - lambda
 #add using lambda :
 add=lambda x,y: x+y
 print(add(1,2))
@@ -7,7 +7,8 @@ print(add(1,2))
 even_odd=lambda x:"even" if x%2==0 else "odd"
 print(even_odd(4))
 
-#map:
+#map: iterates from one variable to another {lambda is like smaller version of function and map is like for loop a sit does the iteration part}
+
 #to find square of all elements in the list
 lt=[1,2,3,4,5]
 sqr=list(map(lambda i: i*i,lt))
@@ -15,5 +16,5 @@ print(sqr)
 
 #type-changing using map
 new=['1','2','3','4']
-a=list(map(int,new))
+a=list(map(int,new))   
 print(a)
