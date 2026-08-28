6883d48ac0da5d78e686235df58da9d43ce18643
saboorkhan6 <saoorkhan6@gmail.com>
2026-08-28 21:43:25 +0530

    commit

--- file-handling/index.py
+++ file-handling/index.py
@@ -22,9 +22,10 @@
 # file.close()
 
 
-# #delete=doesn't work
-# # file=open("hello.txt","x")
-# # file.close()
+# (x=Exclusive Create mode). It is used to create a new file and open it for writing, but it will safely fail and throw a FileExistsError if the file already exists
+#Acts as a safety net. It guarantees that you will never accidentally destroy an existing file's data.
+#  file=open("hello.txt","x")
+#  file.close()
 
 # #write+read+binary format
 # file=open("new.txt","w+b")
@@ -37,5 +38,7 @@ file=open(r"C:\Users\Saboor\OneDrive\Desktop\external\file.txt","w")
 file.write("hello world")
 file.close()
 
+
+#modern way of file-handling: (self-closing)
 with open ("file2.txt","w") as s:
     s.write("modern file handling")
